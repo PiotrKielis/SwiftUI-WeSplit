6ff68535f221affcdfa33b699a8ac1bc23913ca1
PiotrKielis <piter17.02@interia.eu>
2025-07-05 17:01:49 +0200

    Initial Commit

---
+++ SwiftUI WeSplit/SwiftUI_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeSplitApp.swift
+//  SwiftUI WeSplit
+//
+//  Created by Piotr Kieliś on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI WeSplitTests/SwiftUI_WeSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeSplitTests.swift
+//  SwiftUI WeSplitTests
+//
+//  Created by Piotr Kieliś on 05/07/2025.
+//
+
+import Testing
+@testable import SwiftUI_WeSplit
+
+struct SwiftUI_WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
